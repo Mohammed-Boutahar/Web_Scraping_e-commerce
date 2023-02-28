80386363610884cf4899cf361aa97cca309a1d65
Boutahar Mohammed <mohammed.boutahar@mines-ales.org>
2023-02-28 19:21:34 +0100

    organisation scraping

--- scrape_aliexpress_reviews.py
+++ scrape_aliexpress_reviews.py
@@ -26,12 +26,12 @@ while suite == False:
 
 
 #demander l'élement à chercher et étudier
-x = input(str("Veuillez entrer le mot recherché : \n"))
+search_text = input(str("Veuillez entrer le mot recherché : \n"))
 
 HEADERS = ({'User-Agent':
             'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
             AppleWebKit/537.36 (KHTML, like Gecko) \
-            Chrome/90.0.4430.212 Safari/537.36',
+            Chrome/110.0.5481.178 Safari/537.36',
             'Accept-Language': 'en-US, en;q=0.5'})
 
 # user define function
@@ -41,69 +41,79 @@ def getdata(url):
     return r.text
 
 def html_code(url):
-  
     # pass the url
     # into getdata function
     htmldata = getdata(url)
     soup = BeautifulSoup(htmldata, 'html.parser')
-  
+
     # display html code
     return (soup)
 
-#driver Chrome
-PATH = "chromedriver.exe"
-driver = webdriver.Chrome(executable_path=PATH)
-
-
-#site a scraper
-driver.get("https://fr.aliexpress.com/")
-#recup titre du site
-print(driver.title)
-print("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-")
-
-
-#acceder a la barre de recherche
-search = driver.find_element(By.NAME, "SearchText")
-
-#chercher dans la barre de recherche
-search.send_keys(x)
-search.send_keys(Keys.RETURN)
-time.sleep(2)
-
-#scroll down to get more articles
-driver.execute_script("window.scrollBy(0, 2000);")
-time.sleep(1)
-
-driver.execute_script("window.scrollBy(2000, 3000);")
-time.sleep(1)
-
-div_element = driver.find_element(By.CLASS_NAME, "list--gallery--34TropR" )
-link_elements = div_element.find_elements(By.TAG_NAME, 'a')
-
-#stock tous les liens dans une liste et evite les None
-href_list = []
-for link_element in link_elements:
-    href = link_element.get_attribute('href')
-    if href not in [None, "None"]:
-        href_list.append(href)
-
-#taille de la liste
-print(len(href_list))
-
-#affiche que les liens utiles 
+#liste contenant les liens utilent
 href_clean=[]
-j=1
-for i in range(len(href_list)):
-    if i % 2 == 0:
-        print("Lien num", j, " = ", href_list[i], "\n")
-        href_clean.append(href_list[i])
-        j+=1
+def Aliexpress_scraping(x):
+    #driver Chrome
+    PATH = "chromedriver.exe"
+    driver = webdriver.Chrome(executable_path=PATH)
 
-print("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-")
+    #site a scraper
+    driver.get("https://fr.aliexpress.com/")
 
-print("on a toruvé", len(href_list), "liens dans cette page, dont", len(href_clean), "sont utiles")
+    print("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-")
 
 
+    #acceder a la barre de recherche
+    search = driver.find_element(By.NAME, "SearchText")
+
+    #chercher dans la barre de recherche
+    search.send_keys(x)
+    search.send_keys(Keys.RETURN)
+    time.sleep(2)
+
+    #scroll down to get more articles
+    driver.execute_script("window.scrollBy(0, 2000);")
+    time.sleep(1)
+
+    driver.execute_script("window.scrollBy(2000, 3000);")
+    time.sleep(1)
+
+    div_element = driver.find_element(By.CLASS_NAME, "list--gallery--34TropR" )
+    link_elements = div_element.find_elements(By.TAG_NAME, 'a')
+
+    #stock tous les liens dans une liste et evite les None
+    href_list = []
+    for link_element in link_elements:
+        href = link_element.get_attribute('href')
+        if href not in [None, "None"]:
+            href_list.append(href)
+
+    #taille de la liste
+    print(len(href_list))
+
+    #affiche que les liens utiles 
+    j=1
+    for i in range(len(href_list)):
+        if i % 2 == 0:
+            print("Lien num", j, " = ", href_list[i], "\n")
+            href_clean.append(href_list[i])
+            j+=1
+
+    print("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-")
+
+    print("on a trouvé", len(href_list), "liens dans cette page, dont", len(href_clean), "sont utiles")
+
+    driver.quit()
+
+
+if site.lower() == "amazon":
+    pass
+    #    Amazon_scraping(x) a definire apres
+elif site.lower() == "aliexpress":
+    Aliexpress_scraping(search_text)
+
+
+
+print(html_code(href_clean[0]))
 
 
 
@@ -120,7 +130,6 @@ print("on a toruvé", len(href_list), "liens dans cette page, dont", len(href_cl
 
 # finally:
 #     driver.quit()
-
 
 
 # ID = "id"
